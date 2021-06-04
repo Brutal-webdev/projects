3dcbb23f930b610b903eecf9ac640193152427cb
Dominic Scaccia <dominic.777@live.com>
2021-06-03 19:13:37 -0700

    made the base code to make up my MixedMessages script

--- MixedMessage/script.js
+++ MixedMessage/script.js
@@ -3,10 +3,23 @@ const message1 = [];
 const message2 = [];
 const message3 = [];
 
-// random number generators
+// random number generator
+const randomNumber = (arr) => {
+    return Math.floor(Math.random() * arr.length);
+}
 
-// functions to pull the messages
+// assign variables to random message created
+let randomMsg1 = message1[randomNumber(message1)];
+let randomMsg2 = message2[randomNumber(message2)];
+let randomMsg3 = message3[randomNumber(message3)];
 
-// call functions
+// Create the random message
+let randomMsg = `Hey there ${randomMsg1} \n Your random quote today is: ${randomMsg2} \n Keep on being ${randomMsg3}`
 
 // Output to console
+const messageOutput = () => {
+    return console.log(randomMsg);
+}
+
+// log console to tell user to type the function into the console
+console.log('Hey there type messageOutput() into the console to get your random message today!');

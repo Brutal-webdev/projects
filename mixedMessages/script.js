682a82f54a4659f8afe7fcb29d4fced1e660709e
Dominic Scaccia <dominic.777@live.com>
2021-06-03 19:44:52 -0700

    added more words

--- mixedMessages/script.js
+++ mixedMessages/script.js
@@ -1,7 +1,7 @@
 // Storage for messages
 const message1 = ['Stealthy Ninja', 'Strong Warrior', 'Pro Hacker', 'Ninja Coder', 'Ninja Hacker', 'Pro Coder'];
 const message2 = [];
-const message3 = [];
+const message3 = ['trucking!', 'putting your heart in it!', 'staying positive youll make it!', 'looking up you\'ll get there'];
 
 // random number generator
 const randomNumber = (arr) => {
@@ -14,7 +14,7 @@ let randomMsg2 = message2[randomNumber(message2)];
 let randomMsg3 = message3[randomNumber(message3)];
 
 // Create the random message
-let randomMsg = `Hey there ${randomMsg1} \n Your random quote today is: ${randomMsg2} \n Keep on being ${randomMsg3}`
+let randomMsg = `Hey there ${randomMsg1} \n Your random quote today is: \n "${randomMsg2}" \n Keep on ${randomMsg3}`
 
 // Output to console
 const messageOutput = () => {
@@ -22,5 +22,7 @@ const messageOutput = () => {
 }
 
 // log console to tell user to type the function into the console
-console.log('Hey there type messageOutput() into the console to get your random message today!');
+// console.log('Hey there type messageOutput() into the console to get your random message today!');
+
+// call function
 messageOutput();

5252c80dbd1543105cad71a4562463dd9c3e95ce
Dominic Scaccia <dominic.777@live.com>
2021-06-03 19:51:35 -0700

    finished the mixed messages project

--- mixedMessages/script.js
+++ mixedMessages/script.js
@@ -1,6 +1,7 @@
 // Storage for messages
 const message1 = ['Stealthy Ninja', 'Strong Warrior', 'Pro Hacker', 'Ninja Coder', 'Ninja Hacker', 'Pro Coder'];
-const message2 = [];
+const message2 = ['“The Future Belongs To The Competent. Get Good, Get Better, Be The Best!” – Success Quote By Brian Tracy', ' “For Every Reason It’s Not Possible, There Are Hundreds Of People Who Have Faced The Same Circumstances And Succeeded.” – Jack Canfield',
+'“Things Work Out Best For Those Who Make The Best Of How Things Work Out.” – Positive Quote By John Wooden', '“Fake It Until You Make It! Act As If You Had All The Confidence You Require Until It Becomes Your Reality.” – Brian Tracy'];
 const message3 = ['trucking!', 'putting your heart in it!', 'staying positive youll make it!', 'looking up you\'ll get there'];
 
 // random number generator
@@ -14,7 +15,7 @@ let randomMsg2 = message2[randomNumber(message2)];
 let randomMsg3 = message3[randomNumber(message3)];
 
 // Create the random message
-let randomMsg = `Hey there ${randomMsg1} \n Your random quote today is: \n "${randomMsg2}" \n Keep on ${randomMsg3}`
+let randomMsg = `Hey there ${randomMsg1} \n Your random quote today is: \n ${randomMsg2} \n Keep on ${randomMsg3}`
 
 // Output to console
 const messageOutput = () => {

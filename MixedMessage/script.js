12bdbce01da7349db657e6966513e54eaaff1f55
Dominic Scaccia <dominic.777@live.com>
2021-06-03 18:53:09 -0700

    added storage

--- MixedMessage/script.js
+++ MixedMessage/script.js
@@ -1,7 +1,12 @@
 // Storage for messages
+const message1 = [];
+const message2 = [];
+const message3 = [];
 
-// random number generator
+// random number generators
 
 // functions to pull the messages
+
+// call functions
 
 // Output to console
